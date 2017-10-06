dde6c4fc403f71c468160c74384f99d294dd2f86
Elias Biagioni <eliasbiagioni96@hotmail.com>
2017-10-06 13:05:59 -0300

    Primer commit (acomodado controloador IniciarSesion.php)

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ ini_set('display_errors',1);
 error_reporting(-1);
 
 require_once('controllers/IndexController.php');
+require_once('controllers/FormPaciente.php');
+require_once('controllers/IniciarSesion.php');
 require_once('models/PDORepository.php');
 require_once('models/ConfigurationModule.php');
 require_once('models/Configuration.php');
 require_once('views/TwigView.php');
 require_once('views/Home.php');
-require_once('controllers/FormPaciente.php');
-require_once('controllers/IniciarSesion.php');
 
     
 if(!isset($_GET['action'])){
