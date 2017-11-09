4dab5d6795c5fdfc32fdb4e983a5472b139a354f
Elias Biagioni <eliasbiagioni96@hotmail.com>
2017-11-09 19:22:58 -0300

    Prueba telegram

--- api/controller/ApiController.php
+++ api/controller/ApiController.php
@@ -23,10 +23,10 @@
                     $retorno = json_encode($arregloDeHorarios);
                 }
             }else{
-                $retorno = json_encode(array('error'=>'Ya paso esta fecha'));
+                $retorno = json_encode(['error' => 'Ya paso esta fecha']);
             }
         }else{
-            $retorno = json_encode(array('error'=>'Fecha ingresada invalida'));
+            $retorno = json_encode(['error' => 'Fecha ingresada invalida']);
         }
         return $retorno;
     }
