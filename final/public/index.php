8cbcbfc2f38b769a0262aa14711311ac724e9c5d
BIAGIONI, ELIAS <eliasbiagioni96@hotmail.com>
2018-05-02 13:03:06 -0300

    Update index.php

--- final/public/index.php
+++ final/public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'../https://grupo51.proyecto2017.linti.unlp.edu.ar/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'../https://grupo51.proyecto2017.linti.unlp.edu.ar/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
